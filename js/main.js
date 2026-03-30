b74b8d23207e0ae9a9195f02167b51ad6310b885
s0m1k111 <spavel1207@gmail.com>
2026-03-30 21:09:10 +0300

    spread/rest in JS

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ console.log();
 printUser(user);
 */
 
-console.log("Деструктуризация продукта");
+/*console.log("Деструктуризация продукта");
 
 const product = {
   name: "ПК",
@@ -85,3 +85,101 @@ function printProduct({ name, price, category, inStock }) {
 }
 
 printProduct(product);
+*/
+
+/*console.log("Spread для массивов");
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const combined = [...arr1, ...arr2];
+console.log("Объединённый массив:", combined);
+
+const copy = [...arr1];
+console.log("Копия массива:", copy);
+
+const extended = [0, ...arr1, 7, 8];
+console.log("Расширенный массив:", extended);
+*/
+
+/*console.log("Spread для объектов");
+
+const person = {
+  name: "Иван",
+  age: 30,
+};
+
+const address = {
+  city: "Санкт-Петербург",
+  street: "Невский проспект",
+};
+
+const fullInfo = { ...person, ...address };
+console.log("Полная информация:", fullInfo);
+
+const personCopy = { ...person };
+console.log("Копия объекта:", personCopy);
+
+const updated = { ...person, age: 31, occupation: "Developer" };
+console.log("Обновлённый объект:", updated);
+*/
+
+/*console.log("Rest оператор");
+
+function sum(...numbers) {
+  return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log("Сумма 1,2,3:", sum(1, 2, 3));
+console.log("Сумма 1,2,3,4,5:", sum(1, 2, 3, 4, 5));
+
+const numbers = [10, 20, 30, 40, 50];
+const [first, second, ...rest] = numbers;
+console.log("Первое число:", first);
+console.log("Второе число:", second);
+console.log("Остальные числа:", rest);
+*/
+
+console.log("Практика: spread");
+
+const nums1 = [3, 7, 1];
+const nums2 = [10, 4, 8];
+
+const allNums = [...nums1, ...nums2];
+console.log("Объединённый массив:", allNums);
+
+function findMax(...numbers) {
+  if (numbers.length === 0) {
+    return undefined;
+  }
+
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    const current = numbers[i];
+    if (current > max) {
+      max = current;
+    }
+  }
+
+  return max;
+}
+
+console.log("Максимум из массива:", findMax(...allNums));
+console.log("Максимум из арг:", findMax(5, 12, 9, 1));
+
+const objA = {
+  id: 1,
+  title: "Товар",
+};
+
+const objB = {
+  price: 1500,
+  currency: "РУБЛИКИ",
+};
+
+const merged = {
+  ...objA,
+  ...objB,
+};
+
+console.log("Объединённый объект:", merged);
